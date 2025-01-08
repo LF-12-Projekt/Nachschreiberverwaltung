6804124b170e9b8a82222c2d9be7d81ed8a97c5f
lisettezombou <lisette.zombou@gmail.com>
2025-01-08 17:07:19 +0100

    BE-9 add models to models.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def home():
     return "Hello, World!"
+
+
+
+@main.route('/resit/list/{studentId}')
+def resit_list(studentId):
+
+    return
