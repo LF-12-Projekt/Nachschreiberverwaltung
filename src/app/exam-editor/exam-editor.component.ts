5c23a060be55986f8a870ab28d6a4e06e559c6d7
Nicole Belon <belon@aveato.de>
2025-01-29 16:06:59 +0100

    FE: added routing for all components and extended functionaltiies

--- src/app/exam-editor/exam-editor.component.ts
+++ src/app/exam-editor/exam-editor.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import {FormsModule} from "@angular/forms";
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-exam-editor',
-  imports: [],
+  imports: [NzModalModule, NzDatePickerModule, FormsModule, NzSelectModule, CommonModule],
   templateUrl: './exam-editor.component.html',
   styleUrl: './exam-editor.component.scss'
 })
-export class ExamEditorComponent {
+export class ExamEditorComponent implements OnInit {
+  date: Date = new Date();
+  selectedUsers: string[] = [];
+  users = ['John', 'Jane', 'Tom', 'Alice', 'Bob'];
+  examName: string = "";
 
+  constructor() {
+    
+  }
+  ngOnInit(): void {
+  }
+
+  onChange(result: Date): void {
+    console.log('onChange: ', result);
+  }
+  
+  passData() {
+    return this.selectedUsers;
+  }
+  
+  
 }
