575a0fd0f15981c395db553e1994c9e2def2a35a
lisettezombou <lisette.zombou@gmail.com>
2025-02-26 15:41:54 +0100

    BE-1 update swagger.yml, add new endpoint

--- models.py
+++ models.py
@@ -52,6 +52,52 @@ class ResitExamModel:
         finally:
             connection.close()
 
+    @staticmethod
+    def get_resit_exams_by_course(course_id):
+        connection = get_db_connection()
+        try:
+            with connection.cursor() as cursor:
+                query = """
+                    SELECT r.resitId, r.resitName, r.date, r.time, r.room
+                    FROM ResitExam r
+                    WHERE r.courseId = %s
+                """
+                cursor.execute(query, (course_id,))
+                resit_exams = cursor.fetchall()
+
+                exams = []
+                for exam in resit_exams:
+                    resit_id = exam['resitId']
+                    resit_name = exam['resitName']
+                    date = exam['date']
+                    time = exam['time']
+                    room = exam['room']
+                    # Umwandeln der `time` (timedelta) in Stunden:Minuten:Sekunden
+                    if isinstance(time, timedelta):  # Wenn `time` ein timedelta Objekt ist
+                        total_seconds = int(time.total_seconds())
+                        hours = total_seconds // 3600
+                        minutes = (total_seconds % 3600) // 60
+                        seconds = total_seconds % 60
+                        time_str = f"{hours:02}:{minutes:02}:{seconds:02}"
+                    else:
+                        time_str = time
+
+                    # Umwandeln der `date` in das Format YYYY-MM-DD
+                    date_str = date.strftime('%Y-%m-%d') if isinstance(date, datetime) else date
+
+                    # Hinzufügen der Daten zum Ergebnis
+                    exams.append({
+                        'resitId': resit_id,
+                        'resitName': resit_name,
+                        'date': date_str,
+                        'time': time_str,
+                        'room': room
+                    })
+
+                return exams
+        finally:
+            connection.close()
+
 
 class CourseModel:
     @staticmethod
@@ -73,3 +119,13 @@ class CourseModel:
                 return courses
         finally:
             connection.close()
+
+
+def execute_query(query, params=None):
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query, params)
+            return cursor.fetchall()
+    finally:
+        connection.close()
